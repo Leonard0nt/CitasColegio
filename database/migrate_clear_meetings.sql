191865514d9233d9394003382cc5d00acf43fe8f
Leonard0nt <leopoldo.wall1999@gmail.com>
2026-06-12 08:46:54 -0400

    12-06-26

--- database/migrate_clear_meetings.sql
+++
@@ -1,6 +0,0 @@
--- Elimina todas las reuniones agendadas.
--- Ejecutar solo cuando se quiera limpiar por completo la tabla meetings.
-
-SET FOREIGN_KEY_CHECKS = 0;
-TRUNCATE TABLE meetings;
-SET FOREIGN_KEY_CHECKS = 1;
--- database/migrate_meetings_unique_teacher_datetime.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE meetings
-ADD CONSTRAINT uniq_meetings_teacher_datetime UNIQUE (teacher_id, meeting_date, meeting_time);
